30df2b2dd9468d4c19f45f1c9636dbb76bc18b81
Grant <gmontgomery1994@gmail.com>
2022-05-08 12:44:33 -0700

    go binaries go in bin now.

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/grantmontgomery/grantcreates3.0/server"
 )
 
@@ -11,7 +9,6 @@ import (
 
 func main() {
 	server.RunServer()
-	log.Println("Hello world!")
 }
 
 

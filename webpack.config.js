70249769eb7c5fa4e64e9df882968c64da1153de
Grant <gmontgomery1994@gmail.com>
2022-11-12 12:08:05 -0800

    added css-loader and file-loader to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,33 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.ts",
   context: __dirname,
+  resolve: {
+    extensions: ["*", ".js", ".ts"],
+    modules: [__dirname, "src", "node_modules"],
+  },
   output: {
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
+      {
+        test: /\.(css|scss)$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName:
+                  process.env.NODE_ENV === "development"
+                    ? "[name]__[local]--[hash:base64:5]"
+                    : "[hash:base64:5]",
+              },
+            },
+          },
+        ],
+      },
       {
         test: /\.ts$/,
         exclude: [/node_modules/],
@@ -15,6 +37,10 @@ module.exports = {
         options: {
           transpileOnly: true,
         },
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
       },
     ],
   },
